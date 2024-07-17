a85c407bc8830de256e1a4dd60431a57a6a31bd4
Aashish <techaashish05@gmail.com>
2024-07-17 18:14:32 +0530

    fixed bottom nav bar

--- app/src/main/java/com/android/kotemanagement/fragments/users/AddUserFragment.java
+++ app/src/main/java/com/android/kotemanagement/fragments/users/AddUserFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 
 import com.android.kotemanagement.R;
+import com.android.kotemanagement.modals.ViewUserModal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddUserFragment extends Fragment {
+  EditText etUserID;
+  EditText etUserName;
+  EditText etDateOfJoining;
+  AppCompatSpinner spinnerRank;
+  Button btnAddUser;
+
+  List<String> ranks = new ArrayList<>();
 
   @Nullable
   @Override
@@ -20,6 +35,33 @@ public class AddUserFragment extends Fragment {
       @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_add_user, container, false);
+    etUserID = view.findViewById(R.id.teUserID);
+    etUserName = view.findViewById(R.id.teUserName);
+    etDateOfJoining = view.findViewById(R.id.teDateOfJoining);
+    spinnerRank = view.findViewById(R.id.spinnerRank);
+    btnAddUser = view.findViewById(R.id.btnAddUser);
+
+    ArrayAdapter<CharSequence> arrayAdapter =
+        ArrayAdapter.createFromResource(
+            view.getContext(), R.array.ranks_array, android.R.layout.simple_spinner_item);
+    spinnerRank.setAdapter(arrayAdapter);
+    btnAddUser.setOnClickListener(
+            v -> {
+              String id = etUserID.getText().toString();
+              String name = etUserName.getText().toString();
+              String rank = spinnerRank.getSelectedItem().toString();
+              String dateOfJoining = etDateOfJoining.getText().toString();
+
+              if (!id.isEmpty() && !name.isEmpty() && !rank.isEmpty() && !dateOfJoining.isEmpty()) {
+
+                ViewUserModal viewUserModal = new ViewUserModal(id, name, rank, dateOfJoining);
+                ViewUserFragment.viewUserModalList.add(viewUserModal);
+                etUserID.setText("");
+                etUserName.setText("");
+                etDateOfJoining.setText("");
+                spinnerRank.setSelection(0);
+              }
+            });
     return view;
   }
 }
--- app/src/main/java/com/android/kotemanagement/fragments/users/ViewUserFragment.java
+++ app/src/main/java/com/android/kotemanagement/fragments/users/ViewUserFragment.java
@@ -1,6 +1,7 @@
 package com.android.kotemanagement.fragments.users;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +19,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViewUserFragment extends Fragment {
-  List<ViewUserModal> viewUserModalList = new ArrayList<>();
+  static List<ViewUserModal> viewUserModalList = new ArrayList<>();
   RecyclerView rvUsers;
+  ViewUserAdapter viewUserAdapter;
 
   @Nullable
   @Override
@@ -29,16 +31,16 @@ public class ViewUserFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_view_user, container, false);
     rvUsers = (RecyclerView) view.findViewById(R.id.rvUsers);
-
-    ViewUserModal viewUserModal = new ViewUserModal("1", "Aashish", "Corporal", "12-07-2021");
-    viewUserModalList.add(viewUserModal);
-    ViewUserModal viewUserModal1 = new ViewUserModal("2", "Name", "Corporal", "12-07-2021");
-    viewUserModalList.add(viewUserModal1);
-
-    ViewUserAdapter viewUserAdapter = new ViewUserAdapter(viewUserModalList);
+    viewUserAdapter = new ViewUserAdapter(viewUserModalList);
     rvUsers.setLayoutManager(new LinearLayoutManager(getContext()));
     rvUsers.setAdapter(viewUserAdapter);
+    Log.d("TAG", "onCreateView: " + viewUserModalList.size());
 
     return view;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
   }
 }
