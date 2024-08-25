cb0082a1d154af0632a944c443ca45395df965a7
ABHIJEET <abhijeet.bruce@gmail.com>
2024-08-25 21:05:30 +0530

    Android Room, KSP dependencies have been added added and database package have been created.

---
+++ app/src/main/java/com/android/kotemanagement/roomdatabase/dao/SoldiersDAO.kt
@@ -0,0 +1,29 @@
+package com.android.kotemanagement.roomdatabase.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.android.kotemanagement.roomdatabase.entities.Soldiers
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SoldiersDAO {
+
+    @Insert
+    fun insert(soldier: Soldiers)
+
+    @Delete
+    fun delete(soldier: Soldiers)
+
+    @Update
+    fun update(soldier: Soldiers)
+
+    @Query("SELECT * FROM soldiers_table")
+    fun getAllSoldiers() : Flow<List<Soldiers>>
+
+    @Query("SELECT * FROM soldiers_table WHERE armyNumber = :armyNumber")
+    fun getSoldierByArmyNumber(armyNumber: String) : Soldiers
+
+}
